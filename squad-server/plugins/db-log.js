46d726e8f026ed36c9bc39cc6e74a089558936f9
Misha Yudin <misha.yudin19@gmail.com>
2025-06-12 14:26:05 +0300

    Move country resolver to utils

--- squad-server/plugins/db-log.js
+++ squad-server/plugins/db-log.js
@@ -4,47 +4,9 @@ import BasePlugin from './base-plugin.js';
 
 import FactionSides from './lookup/factions-lookup.js';
 
-import https from 'https';
+import getCountryByIP from '../utils/ip-api.js'
 
 const { DataTypes, QueryTypes } = Sequelize;
-
-function getCountryByIP(ip) {
-  return new Promise((resolve) => {
-    const options = {
-      hostname: 'api.country.is',
-      path: `/${ip}`,
-      method: 'GET',
-    };
-
-    const req = https.request(options, (res) => {
-      if (res.statusCode !== 200) {
-        // console.debug('Non-success status:', res.statusCode);
-        return resolve(null);
-      }
-
-      let data = '';
-      res.on('data', (chunk) => data += chunk);
-      res.on('end', () => {
-        try {
-          const json = JSON.parse(data);
-          const country = json.country;
-          const flag = country.toUpperCase().split('').map(char => String.fromCodePoint(char.charCodeAt(0) + 127397)).join('') || '';
-          resolve(country + flag);
-        } catch (e) {
-          // console.debug('JSON parse error:', e);
-          resolve(null);
-        }
-      });
-    });
-
-    req.on('error', (err) => {
-      // console.debug('Request error:', err);
-      resolve(null);
-    });
-
-    req.end();
-  });
-}
 
 export default class DBLog extends BasePlugin {
   static get description() {
---
+++ squad-server/utils/ip-api.js
@@ -0,0 +1,39 @@
+import https from 'https';
+
+export async function getCountryByIP(ip) {
+  return new Promise((resolve) => {
+    const options = {
+      hostname: 'api.country.is',
+      path: `/${ip}`,
+      method: 'GET',
+    };
+
+    const req = https.request(options, (res) => {
+      if (res.statusCode !== 200) {
+        // console.debug('Non-success status:', res.statusCode);
+        return resolve(null);
+      }
+
+      let data = '';
+      res.on('data', (chunk) => data += chunk);
+      res.on('end', () => {
+        try {
+          const json = JSON.parse(data);
+          const country = json.country;
+          const flag = country.toUpperCase().split('').map(char => String.fromCodePoint(char.charCodeAt(0) + 127397)).join('') || '';
+          resolve(country + flag);
+        } catch (e) {
+          // console.debug('JSON parse error:', e);
+          resolve(null);
+        }
+      });
+    });
+
+    req.on('error', (err) => {
+      // console.debug('Request error:', err);
+      resolve(null);
+    });
+
+    req.end();
+  });
+}
